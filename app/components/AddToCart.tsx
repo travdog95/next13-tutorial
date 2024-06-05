453232927d410b22c1ebf10bd6fb27623fb84828
Travis Peters <Travis.Peters@ivanti.com>
2024-06-05 12:50:33 -0600

    initial commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <button onClick={() => console.log("add to cart")}>Add to cart</button>
+  );
+};
+
+export default AddToCart;
